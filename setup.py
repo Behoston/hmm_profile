722c47ca1aa5b071001951d91e1e82dd63493a0e
Behoston <mlegiecki@gmail.com>
2020-02-19 22:37:15 +0100

    Back to development: 0.0.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ else:
 setup(
     name='hmm_profile',
     packages=find_packages(exclude=['tests']),
-    version='0.0.1',
+    version='0.0.2.dev0',
     url='https://github.com/Behoston/hmm_profile',
     license='MIT',
     author='Behoston',
